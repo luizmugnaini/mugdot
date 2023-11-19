800adeecbb5876e34d097a791a2aa3d1ace42e50
Luiz Mugnaini <luizmugnaini@gmail.com>
2023-11-19 14:01:01 -0300

    Fix comment.nvim
    Signed-off-by: Luiz Mugnaini <luizmugnaini@gmail.com>

--- nvim/after/plugin/comment.lua
+++ nvim/after/plugin/comment.lua
@@ -1,11 +1,6 @@
-require("Comment").setup {
-    toggler = {
-        line = "<Space>lc",
-        block = "<Space>bc",
-    },
-    -- LHS of operator-pending mappings in NORMAL and VISUAL mode
-    opleader = {
-        line = "<Space>lc",
-        block = "<Space>bc",
-    },
-}
+require("Comment").setup({
+	toggler = {
+		line = "<Space>lc",
+		block = "<Space>bc",
+	},
+})
