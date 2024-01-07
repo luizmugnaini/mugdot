3946a084969aad2f2e6f0b980d59ec20135e74f9
Luiz Mugnaini <luizmugnaini@gmail.com>
2024-01-07 09:55:59 -0300

    Fix dwmblocks
    Signed-off-by: Luiz Mugnaini <luizmugnaini@gmail.com>

--- dwm/dwmblocks/blocks.def.h
+++ dwm/dwmblocks/blocks.def.h
@@ -1,12 +1,12 @@
 static const Block blocks[] = {
-  /*Icon*/	/*Command*/		/*Update Interval*/	/*Update Signal*/
-  {"", "~/.mugdot/dwm/dwmblocks/scripts/wlan.sh", 5,  0},
+    /*Icon*/ /*Command*/ /*Update Interval*/ /*Update Signal*/
+    {"", "~/Projects/mugdot/dwm/dwmblocks/scripts/wlan.sh", 5, 0},
 
-  {"", "~/.mugdot/dwm/dwmblocks/scripts/vol.sh",   5,  0},
+    {"", "~/Projects/mugdot/dwm/dwmblocks/scripts/vol.sh", 5, 0},
 
-  {"", "~/.mugdot/dwm/dwmblocks/scripts/bat.sh",  30,  0},
+    {"", "~/Projects/mugdot/dwm/dwmblocks/scripts/bat.sh", 30, 0},
 
-  {"", "~/.mugdot/dwm/dwmblocks/scripts/date.sh",  5,  0},
+    {"", "~/Projects/mugdot/dwm/dwmblocks/scripts/date.sh", 5, 0},
 };
 
 // sets delimeter between status commands. NULL character ('\0') means no delimeter.
