fc28d682cefdca285900df2490930098bb27efd0
Luiz G. Mugnaini A. <luizmugnaini@gmail.com>
2024-03-03 14:23:04 -0300

    Add cmder config
    Signed-off-by: Luiz G. Mugnaini A. <luizmugnaini@gmail.com>

---
+++ cmder/user_aliases.cmd
@@ -0,0 +1,19 @@
+;= @echo off
+;= rem Call DOSKEY and use this file as the macrofile
+;= %SystemRoot%\system32\doskey /listsize=1000 /macrofile=%0%
+;= rem In batch mode, jump to the end of the file
+;= goto:eof
+;= Add aliases below here
+e.=explorer .
+pwd=cd
+clear=cls
+vi=vim $*
+v=nvim $*
+vim=nvim $*
+cmderr=cd /d "%CMDER_ROOT%"
+pwsh=%SystemRoot%/System32/WindowsPowerShell/v1.0/powershell.exe -ExecutionPolicy Bypass -NoLogo -NoProfile -NoExit -Command "Invoke-Expression '. ''%CMDER_ROOT%/vendor/profile.ps1'''"
+l=eza --icons --group-directories-first
+la=eza --icons --group-directories-first -a
+ll=eza --long --icons --git --group-directories-first
+lla=eza --long --icons --git --group-directories-first -a
+raddbg=%HOME%\Programs\raddebugger\build\raddbg.exe
