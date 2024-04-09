c4e1c228f9e895b650886c86876b3c7bf76f0e27
Luiz Mugnaini <luizmugnaini@gmail.com>
2024-04-09 11:34:28 -0300

    Nvim leap plugin
    Signed-off-by: Luiz Mugnaini <luizmugnaini@gmail.com>

---
+++ nvim/after/plugin/theme.lua
@@ -0,0 +1,5 @@
+vim.g.gruvbox_material_better_performance = 1
+vim.g.gruvbox_material_foreground = "material" -- choices are "material", "mix", "original"
+vim.g.gruvbox_material_background = "hard"
+vim.g.gruvbox_material_disable_italic_comment = 1
+vim.cmd.colorscheme("gruvbox-material")
