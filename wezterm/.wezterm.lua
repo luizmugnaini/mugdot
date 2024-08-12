44304845f4b4571fd7f4a9135b6748320ac52648
Luiz Mugnaini <luizmugnaini@gmail.com>
2024-08-12 12:53:39 -0300

    wezterm: add keys for split, changing tabs, separate windows/linux
    Signed-off-by: Luiz Mugnaini <luizmugnaini@gmail.com>

--- wezterm/.wezterm.lua
+++ wezterm/.wezterm.lua
@@ -6,15 +6,27 @@
 
 local wezterm = require("wezterm")
 local config = wezterm.config_builder()
-local mux = wezterm.mux
 
--- Start in fullscreen.
-wezterm.on("gui-startup", function(cmd)
-    local tab, pane, window = mux.spawn_window({ cmd or {} })
-    window:gui_window():maximize()
-end)
+if wezterm.target_triple == "x86_64-pc-windows-msvc" then
+    config.font = wezterm.font("Terminus (TTF) for Windows")
 
-config.font = wezterm.font("Terminus (TTF) for Windows")
+    config.default_prog = {
+        "cmd.exe",
+        "/k",
+        "%CMDER_ROOT%\\vendor\\init.bat",
+        "&&",
+        "'C:\\Program Files\\Microsoft Visual Studio\\2022\\Community\\VC\\Auxiliary\\Build\\vcvarsall.bat'",
+        "x64",
+        "> NUL",
+    }
+    config.default_cwd = "D:\\"
+else
+    config.font = wezterm.font("Terminus (TTF)")
+
+    config.default_prog = { "zsh" }
+    config.default_cwd = "~/"
+end
+
 config.font_size = 12
 config.window_frame = { font = config.font, font_size = 9 }
 config.colors = {
@@ -22,13 +34,35 @@ config.colors = {
     foreground = "#ffffff",
 }
 
-config.default_prog = {
-    "cmd.exe",
-    "/k",
-    "%CMDER_ROOT%\\vendor\\init.bat",
-    "&&",
-    '"C:\\Program Files\\Microsoft Visual Studio\\2022\\Community\\VC\\Auxiliary\\Build\\vcvarsall.bat" x64 > NUL',
+config.leader = { key = "Space", mods = "CTRL" }
+config.keys = {
+    {
+        key = "f",
+        mods = "LEADER",
+        action = wezterm.action.ToggleFullScreen,
+    },
+    {
+        key = "r",
+        mods = "LEADER",
+        action = wezterm.action.ReloadConfiguration,
+    },
+    {
+        key = "o",
+        mods = "LEADER",
+        action = wezterm.action.ActivateLastTab,
+    },
+    {
+        key = "v",
+        mods = "LEADER",
+        action = wezterm.action.SplitVertical({ domain = "CurrentPaneDomain" }),
+    },
 }
-config.default_cwd = "D:/"
+for tab_idx = 1, 9 do
+    table.insert(config.keys, {
+        key = tostring(tab_idx),
+        mods = "LEADER",
+        action = wezterm.action.ActivateTab(tab_idx - 1),
+    })
+end
 
 return config
